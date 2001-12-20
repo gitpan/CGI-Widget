dcc1ff63335f6a9e898a33a0b30b63c2dfe656ca
Allen Day <allenday@cpan.org>
2001-12-20 03:49:34 +0000

    Import of ALLENDAY/CGI-Widget-0.01 from CPAN.
    gitpan-cpan-distribution: CGI-Widget
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         ALLENDAY/CGI-Widget-0.01.tar.gz
    gitpan-cpan-author:       ALLENDAY
    gitpan-cpan-maturity:     released

---
+++ Widget.pm
@@ -0,0 +1,60 @@
+package CGI::Widget;
+
+use strict;
+use warnings;
+use overload
+ '""'     => \&asString,
+ fallback => 1;
+
+our $VERSION = '0.01';
+
+sub new {
+  my $class = shift;
+  my $self = bless {}, $class;
+  $self->_init(@_);
+  return $self;
+}
+
+sub _init {
+  my $self = shift;
+  return 1;
+}
+
+sub asString {
+  my $self = shift;
+  return $self;  #what do you expect from the base class?
+}
+
+1;
+__END__
+# Below is stub documentation for your module. You better edit it!
+
+=head1 NAME
+
+CGI::Widget - Base class for CGI::Widget::...
+
+=head1 SYNOPSIS
+
+  use CGI::Widget;
+  blah blah blah
+
+=head1 DESCRIPTION
+
+You probably don't want to create a new CGI::Widget object, unless
+you're subclassing.  This is just a base class.
+
+=head2 EXPORT
+
+None by default.
+
+=head1 AUTHOR
+
+ Allen Day E<allenday@ucla.edu>
+ Copyright (c) 2001.
+
+=head1 SEE ALSO
+
+L<Perl>.
+L<CGI::Widget::Series>
+
+=cut
---
+++ t/t.t
@@ -0,0 +1,9 @@
+#!/usr/bin/perl -w
+#this file here just to shut up 'make test' errors 
+use strict;
+use Test;
+
+BEGIN { plan tests => 1 }
+
+ok(1);
+1;
